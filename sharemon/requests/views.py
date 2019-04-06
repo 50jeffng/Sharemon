bde9c5a06bdcaf7c1039aa4caeed28300e77bb6c
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-04-06 14:53:46 +1100

    add Student, Post in models; make a test html

--- sharemon/requests/views.py
+++ sharemon/requests/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = models.Post.objects.all()
+    return render(request, "posts/post_list.html", {'posts': posts})
+
